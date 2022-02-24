025983b74190d73b32c08acd2d20a17da346c496
Qien Nathan Cai <97143818+nathan-cai@users.noreply.github.com>
2022-02-24 15:13:31 -0800

    Delete DJango/lecture3 directory
    Restarting Lesson

--- DJango/lecture3/hello/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from numpy import greater_equal
-from . import views
-
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("nathan", views.nathan, name="nathan"),
-    path('<str:name>', views.greet, name='greet')
-]
